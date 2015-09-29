0ea335c31518b5265cd5edfeacb41c2f671d96fd
Chad Mazilly <chadmazilly@gmail.com>
2015-09-29 09:06:26 -0500

    bumped version number

--- version.php
+++ version.php
@@ -4,5 +4,9 @@
  */
 defined('MOODLE_INTERNAL') or die();
 
-$plugin->version = 2013081007;
-$plugin->cron = 43200;
+$plugin->component = 'enrol_ues';
+$plugin->version = 2015092308;
+$plugin->requires = 2015051102;
+$plugin->cron = 43200; // @todo - remove this in favor of the scheduled task API
+// $plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v3.0.0';
