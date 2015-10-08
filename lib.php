219e4fc79929c8b59fa6497a7cc369982842d66b
Robert Russo <rrusso@lsu.edu>
2015-10-08 08:57:57 -0500

    Fix for email logs not adhering to settings.

--- lib.php
+++ lib.php
@@ -330,7 +330,7 @@ class enrol_ues_plugin extends enrol_plugin {
 
         $admins = get_admins();
 
-        if ($this->setting('email_report') and !empty($this->emaillog)) {
+        if ($this->setting('email_report')) {
             $email_text = implode("\n", $this->emaillog);
 
             foreach ($admins as $admin) {
