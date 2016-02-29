ff50c73b6ec448a6156cc6024d43c0342fa14042
Chad Mazilly <chadmazilly@gmail.com>
2016-02-29 15:12:16 -0600

    bump up ues version number

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') or die();
 
 $plugin->component = 'enrol_ues';
-$plugin->version = 2015100711;
+$plugin->version = 2016022912;
 $plugin->requires = 2015051102;
 // $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v3.0.0';
