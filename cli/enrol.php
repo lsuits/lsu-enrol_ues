90ae1df08426f6a32ba787ae2657e98fb8e9c482
Chad Mazilly <chadmazilly@gmail.com>
2016-03-01 14:29:52 -0600

    add a cli option for manual enrollment processing

---
+++ cli/enrol.php
@@ -0,0 +1,40 @@
+<?php
+
+define('CLI_SCRIPT', true);
+
+require_once '../../../config.php';
+global $CFG;
+require_once($CFG->libdir.'/clilib.php');
+require_once($CFG->dirroot . '/enrol/ues/lib.php');
+
+// get cli options
+list($options, $unrecognized) = cli_get_params(array('help'=>false), array('h'=>'help'));
+
+if ($unrecognized) {
+    $unrecognized = implode("\n  ", $unrecognized);
+    cli_error(get_string('cliunknowoption', 'admin', $unrecognized));
+}
+
+if ($options['help']) {
+    echo "
+****************************
+UES Enrollment CLI commands.
+****************************
+
+Options:
+-h, --help            Print out this help
+
+Example:
+\$sudo -u www-data /usr/bin/php enrol/ues/cli/enrol.php
+
+";
+    die;
+}
+
+$ues = new \enrol_ues_plugin();
+
+$response = $ues->full_process();
+
+echo 'all done!
+';
+die;
