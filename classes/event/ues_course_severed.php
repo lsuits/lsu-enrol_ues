aaf4839ffcb3d028ccd57095e939ed51f307645b
Chad Mazilly <chadmazilly@gmail.com>
2016-03-29 09:11:27 -0500

    bug fixes

--- classes/event/ues_course_severed.php
+++ classes/event/ues_course_severed.php
@@ -4,7 +4,7 @@ namespace enrol_ues\event;
 
 defined('MOODLE_INTERNAL') || die();
 
-class use_course_severed extends \enrol_ues\event\ues_event_base {
+class ues_course_severed extends \enrol_ues\event\ues_event_base {
 
     /**
      * Initialize the event
