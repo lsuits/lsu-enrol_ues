c6ad78c487d3c095227d4d2c3e1dfb74f9ff54bc
Chad Mazilly <chadmazilly@gmail.com>
2016-04-08 11:11:49 -0500

    bump ver

--- version.php
+++ version.php
@@ -1,11 +1,14 @@
 <?php
+
 /**
- * @package enrol_ues
+ * @package   enrol_ues
+ * @copyright 2016, Louisiana State University
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-defined('MOODLE_INTERNAL') or die();
+
+defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'enrol_ues';
-$plugin->version = 2015100711;
-$plugin->requires = 2015051102;
-// $plugin->maturity = MATURITY_ALPHA;
+$plugin->version = 2016040800;
+$plugin->requires = 2015111600;
 $plugin->release = 'v3.0.0';
