c23a28163b19058834cca36dc9656d76d4aef911
Chad Mazilly <chadmazilly@gmail.com>
2016-04-14 09:50:43 -0500

    add new required override method for access check

--- lib.php
+++ lib.php
@@ -1823,6 +1823,16 @@ class enrol_ues_plugin extends enrol_plugin {
 
         $this->emaillog[] = $what;
     }
+
+    /**
+     * Is it possible to hide/show enrol instance via standard UI?
+     *
+     * @param stdClass $instance
+     * @return bool
+     */
+    public function can_hide_show_instance($instance) {
+        return is_siteadmin();
+    }
 }
 
 function enrol_ues_supports($feature) {
