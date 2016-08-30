23dcc2254560add0bf52295f36241c6cbfd911a7
Chad Mazilly <chadmazilly@gmail.com>
2016-08-30 13:29:37 -0500

    bump ver, add component name

--- version.php
+++ version.php
@@ -4,5 +4,8 @@
  */
 defined('MOODLE_INTERNAL') or die();
 
-$plugin->version = 2013081007;
-$plugin->cron = 43200;
+$plugin->component = 'enrol_ues';
+$plugin->version = 2016083000;
+$plugin->requires = 2015051102;
+$plugin->cron = 43200; // @TODO - remove this in favor of the scheduled task API
+$plugin->release = 'v3.1.0';
